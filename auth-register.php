be61916da0748d4f5f8834e07f20f9e77dc4b42e
richiesuper <richie9963@gmail.com>
2023-04-30 23:29:37 +0700

    feat: add auth-register.php

---
+++ auth-register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Silaturahmi Online Bareng Cak Eri</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+	<?php
+
+	include "navbar.php";
+
+	if (isset($_GET['rstatus']) && $_GET['rstatus'] === "failed") {
+			echo "<h1>Registration failed. Please try to register again.";
+	}
+
+	echo '
+		<form action="register.php" method="POST">
+			<label for="email">Email</label>
+			<input id="email" name="email" type="text" placeholder="Email">
+			<label for="username">Username</label>
+			<input id="username" name="username" type="text" placeholder="Username">
+			<label for="password">Password</label>
+			<input id="password" name="password" type="password" placeholder="Password">
+			<label for="type">Account Type</label>
+			<select id="type" name="type">
+				<option value="P">Pejabat</option>
+				<option value="R">Rakyat</option>
+			</select>
+			<button name="register" value="register">Register</button>
+		</form>
+	';
+
+	?>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
