9f3c6441f93dc65534a0a6bacfc441acdac87030
richiesuper <richie9963@gmail.com>
2023-04-30 17:22:30 +0700

    feat: add config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "silaturahmi";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+if (!$conn) {
+	die("Failed to connect to database: " . mysqli_connect_error());
+}
+
+?>
