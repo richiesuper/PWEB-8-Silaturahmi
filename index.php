2b3bec05d04be46aaa9f15a453cbb209e1c1f230
richiesuper <richie9963@gmail.com>
2023-04-30 23:28:37 +0700

    feat: update index.php

--- index.php
+++ index.php
@@ -9,27 +9,8 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
-<body class="d-flex flex-column">
-	<header>
-		<nav class="navbar navbar-expand-lg bg-primary">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="/">Silaturahmi</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav ms-auto">
-						<li class="nav-item">
-							<a class="nav-link" aria-current="page" href="auth.php">Daftar Ucapan</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Login</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</header>
+<body>
+	<?php include "navbar.php"; ?>
 	<main>
 		<img src="assets/img/banner.jpeg" class="w-100" alt="Banner image">
 	</main>
